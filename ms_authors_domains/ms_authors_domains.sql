c70fd6eae31e350fab93aabcfa45cbc66f3ab6db
AntoniaGogoglou <gogoant@gmail.com>
2017-11-02 12:00:14 +0200

    added ms_authors_domains.sql

---
+++ ms_authors_domains/ms_authors_domains.sql
@@ -0,0 +1,40 @@
+-- MySQL dump 10.13  Distrib 5.6.38, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: dblp_antonia
+-- ------------------------------------------------------
+-- Server version	5.6.38-1~dotdeb+7.1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ms_authors_domains`
+--
+
+DROP TABLE IF EXISTS `ms_authors_domains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ms_authors_domains` (
+  `msauthor_id` bigint(20) unsigned NOT NULL,
+  `dom_order` tinyint(1) unsigned NOT NULL,
+  `mssubdomain_id` int(11) unsigned DEFAULT NULL,
+  `msdomain_id` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`msauthor_id`,`dom_order`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-01 21:46:08
